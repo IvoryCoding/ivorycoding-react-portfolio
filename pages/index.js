9b9546c3655b3a9820914479c6e2295be3fae625
sosumi <67837670+IvoryCoding@users.noreply.github.com>
2022-10-28 16:08:48 -0600

    remove blog button
    blog has not been created yet therefore I am removing the button for it.

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">aka IvoryCoding</h1>
             <ul className="flex items-center">
-              <li><a href="#" className="bg-gradient-to-r from-yellow-600 to-yellow-900 text-white px-4 py-2 rounded-mb ml-8">My Blog</a></li>
+              {/*<li><a href="#" className="bg-gradient-to-r from-yellow-600 to-yellow-900 text-white px-4 py-2 rounded-mb ml-8">My Blog</a></li>*/}
             </ul>
           </nav>
 
